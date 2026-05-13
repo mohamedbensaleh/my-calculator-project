cb65d63d075e92a335b8e70b4cae63f9064811ee
mohamedbensaleh <mohamedbensaleh.contact@gmail.com>
2026-05-13 15:31:21 +0100

    Added power function-branch

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+#import matplotlib
 # Simple Calculator - Version 3
 def add(a, b):
     return a + b
@@ -11,8 +12,12 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
+def power(base, exponent):
+    return base ** exponent
+
 # Test all functions
 print("1 + 2 =", add(1, 2))
 print("5 - 3 =", subtract(5, 3))
 print("2 * 3 =", multiply(2, 3))
 print("10 / 2 =", divide(10, 2))
+print("2^3 =", power(2, 3))

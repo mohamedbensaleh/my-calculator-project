58da0b3fc22f32e5d677c88af18b981d4cfca87e
mohamedbensaleh <mohamedbensaleh.contact@gmail.com>
2026-05-13 14:54:01 +0100

    Added subtract function

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
-# Simple Calculator - Version 1
+# Simple Calculator - Version 2
 def add(a, b):
     return a + b
 
-# Test the function
+def subtract(a, b):
+    return a - b
+
+# Test functions
 print("1 + 2 =", add(1, 2))
+print("5 - 3 =", subtract(5, 3))

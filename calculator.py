708043a060ce7e41915c22b82e2b8f3441b98cf2
mohamedbensaleh <mohamedbensaleh.contact@gmail.com>
2026-05-13 14:55:43 +0100

    Added multiply and divide functions

--- calculator.py
+++ calculator.py
@@ -1,10 +1,18 @@
-# Simple Calculator - Version 2
+# Simple Calculator - Version 3
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
 
-# Test functions
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+# Test all functions
 print("1 + 2 =", add(1, 2))
 print("5 - 3 =", subtract(5, 3))
+print("2 * 3 =", multiply(2, 3))
+print("10 / 2 =", divide(10, 2))
